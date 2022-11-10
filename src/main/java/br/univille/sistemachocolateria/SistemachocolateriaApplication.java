76021f91c6af5492e4fe1babd7c5bc40bf19f21e
ALDINÉIA DE MORAIS MATTOS <aldineia.mattos@univille.br>
2022-11-10 14:19:50 -0300

    first commit

---
+++ src/main/java/br/univille/sistemachocolateria/SistemachocolateriaApplication.java
@@ -0,0 +1,13 @@
+package br.univille.sistemachocolateria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemachocolateriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemachocolateriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/sistemachocolateria/SistemachocolateriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.sistemachocolateria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemachocolateriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
