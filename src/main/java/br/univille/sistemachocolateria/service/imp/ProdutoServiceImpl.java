cd2284bdcbb858e154234ff9c19df258de88f4dc
ALDINÉIA DE MORAIS MATTOS <aldineia.mattos@univille.br>
2022-11-16 14:34:21 -0300

    cadastro de produtos

---
+++ src/main/java/br/univille/sistemachocolateria/service/imp/ProdutoServiceImpl.java
@@ -0,0 +1,42 @@
+package br.univille.sistemachocolateria.service.imp;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.univille.sistemachocolateria.entity.Produto;
+import br.univille.sistemachocolateria.repository.ProdutoRepository;
+import br.univille.sistemachocolateria.service.ProdutoService;
+
+@Service
+public class ProdutoServiceImpl
+        implements ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repositorio;
+
+    @Override
+    public List<Produto> getAll() {
+        return repositorio.findAll();
+    }
+
+    @Override
+    public void save(Produto produto) {
+        repositorio.save(produto);
+    }
+
+    @Override
+    public Produto findById(long id) {
+        var resultado = repositorio.findById(id);
+        if (resultado.isPresent()) {
+            return resultado.get();
+        }
+        return new Produto();
+    }
+
+    @Override
+    public void delete(long id) {
+        repositorio.deleteById(id);
+
+    }
+
+}
