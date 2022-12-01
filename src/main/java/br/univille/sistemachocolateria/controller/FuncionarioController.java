c0216ce6162dca20fdbd37d811e0cc037793dce1
BRUNA MIRANDA PEREIRA <b.miranda@univille.br>
2022-12-01 15:20:31 -0300

    atualizaçao

--- src/main/java/br/univille/sistemachocolateria/controller/FuncionarioController.java
+++ src/main/java/br/univille/sistemachocolateria/controller/FuncionarioController.java
@@ -13,23 +13,22 @@ import br.univille.sistemachocolateria.entity.Funcionario;
 import br.univille.sistemachocolateria.service.FuncionarioService;
 
 @Controller
-@RequestMapping
+@RequestMapping("/funcionarios")
 public class FuncionarioController {
     
     @Autowired
     private FuncionarioService service;
 
-    @GetMapping("/funcionarios")
+    @GetMapping
     public ModelAndView index(){
         var listaFuncionarios = service.getAll();
         return new ModelAndView("funcionario/index","listaFuncionarios",listaFuncionarios);
     }
-
     @GetMapping("/novo")
     public ModelAndView novo(){
         var funcionario = new Funcionario();
         var listaFuncionarios = service.getAll();
-        HashMap<String, Object> dados = new HashMap<>();
+        HashMap<String,Object> dados = new HashMap<>();
         dados.put("funcionario", funcionario);
         dados.put("listaFuncionarios", listaFuncionarios);
         return new ModelAndView("funcionario/form", dados);
@@ -37,21 +36,21 @@ public class FuncionarioController {
     @PostMapping(params = "form")
     public ModelAndView save(Funcionario funcionario){
         service.save(funcionario);
-        return new ModelAndView("redirect:/franquiados");
+        return new ModelAndView("redirect:/funcionarios");
     }
     @GetMapping("/alterar/{id}")
     public ModelAndView alterar(@PathVariable("id") long id){
         var funcionario = service.findById(id);
         var listaFuncionarios = service.getAll();
         HashMap<String,Object> dados = new HashMap<>();
-        dados.put("franquiado", funcionario);
+        dados.put("funcionario", funcionario);
         dados.put("listaFuncionarios", listaFuncionarios);
-        return new ModelAndView("franquiado/form", dados);
+        return new ModelAndView("funcionario/form", dados);
     }
     @GetMapping("/delete/{id}")
     public ModelAndView delete(@PathVariable("id") long id){
         service.delete(id);
-        return new ModelAndView("redirect:/franquiados");
+        return new ModelAndView("redirect:/funcionarios");
     }
 }
 
--- src/main/java/br/univille/sistemachocolateria/entity/Funcionario.java
+++ src/main/java/br/univille/sistemachocolateria/entity/Funcionario.java
@@ -17,9 +17,52 @@ public class Funcionario {
     private String email;
     private String telefone;
     private String cpf;
-    private String rg;
     private int idade;
     @OneToOne(cascade = CascadeType.ALL, optional = false)
     @JoinColumn(name = "endereco_id")
     private Endereco endereco;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
 }
