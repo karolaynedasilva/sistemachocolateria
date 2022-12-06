ef4b44b8b55f76d73fbc329333ead541bce9e249
BRUNA MIRANDA PEREIRA <b.miranda@univille.br>
2022-12-06 15:23:26 -0300

    funcionario

--- src/main/java/br/univille/sistemachocolateria/entity/Funcionario.java
+++ src/main/java/br/univille/sistemachocolateria/entity/Funcionario.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
